b8b8e19f155720986d1ece172a29135c5abd0bae
HuyHung1408 <dunghuyhung1@gmail.com>
2022-02-23 17:58:43 +0700

    Small changes

--- Fluent Calculator.py
+++ Fluent Calculator.py
@@ -14,12 +14,6 @@ def btnClearDisplay():
     tex_input.set("")
     operator=""
 
-def backspace(self):
-  self.current = self.current[0:len(self.current) -1]
-
-def backspace(self):
-      self.current = self.current[:-1]
-
 def btnEqualsInput():
     try:
       global operator
@@ -51,7 +45,7 @@ cal.title('Fluent Calculator')
 
 operator=""
 tex_input= StringVar()
-cal.geometry('238x320')
+cal.geometry('238x325')
 cal.iconbitmap('Calculator.ico')
 
 cal.grid_columnconfigure(0,weight=1)
@@ -64,10 +58,11 @@ txtDisplay = ttk.Entry(cal, textvariable=tex_input, font='50', justify='right').
 
 #First Column
 btnM=ttk.Button(cal, text="M", style="Accent.TButton", command=change_theme).grid(row=1, column=0, padx= 8, pady= 8, ipadx=1, ipady=1)
-btn7=ttk.Button(cal, text="7", command=lambda:btnClick(7)).grid(row=2, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
-btn4=ttk.Button(cal, text="4", command=lambda:btnClick(4)).grid(row=3, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
-btn1=ttk.Button(cal, text="1", command=lambda:btnClick(1)).grid(row=4, column=0, padx= 8, pady= 5, ipadx=5, ipady=5)
-btn1=ttk.Button(cal, text="0", command=lambda:btnClick(0)).grid(row=5, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+btnM=ttk.Button(cal, text="M", style="Accent.TButton", command=change_theme).grid(row=2, column=0, padx= 8, pady= 8, ipadx=1, ipady=1)
+btn7=ttk.Button(cal, text="7", command=lambda:btnClick(7)).grid(row=3, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+btn4=ttk.Button(cal, text="4", command=lambda:btnClick(4)).grid(row=4, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+btn1=ttk.Button(cal, text="1", command=lambda:btnClick(1)).grid(row=5, column=0, padx= 8, pady= 5, ipadx=5, ipady=5)
+btn1=ttk.Button(cal, text="0", command=lambda:btnClick(0)).grid(row=6, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
 
 #Second Column
 btnM=ttk.Button(cal, text=".", command=lambda:btnClick('.')).grid(row=1, column=1, padx= 8, pady= 8, ipadx=7, ipady=1)
