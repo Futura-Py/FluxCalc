fa9a4058a599ca22b3b4231cab353ae45e7de2eb
HuyHung1408 <dunghuyhung1@gmail.com>
2022-02-23 21:06:55 +0700

    Update

--- Fluent Calculator.py
+++ Fluent Calculator.py
@@ -58,31 +58,32 @@ txtDisplay = ttk.Entry(cal, textvariable=tex_input, font='50', justify='right').
 
 #First Column
 btnM=ttk.Button(cal, text="M", style="Accent.TButton", command=change_theme).grid(row=1, column=0, padx= 8, pady= 8, ipadx=1, ipady=1)
-btn7=ttk.Button(cal, text="7", command=lambda:btnClick(7)).grid(row=2, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
-btn4=ttk.Button(cal, text="4", command=lambda:btnClick(4)).grid(row=3, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
-btn1=ttk.Button(cal, text="1", command=lambda:btnClick(1)).grid(row=4, column=0, padx= 8, pady= 5, ipadx=5, ipady=5)
-btn1=ttk.Button(cal, text="0", command=lambda:btnClick(0)).grid(row=5, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+
+btn7=ttk.Button(cal, text="7", command=lambda:btnClick(7)).grid(row=3, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+btn4=ttk.Button(cal, text="4", command=lambda:btnClick(4)).grid(row=4, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
+btn1=ttk.Button(cal, text="1", command=lambda:btnClick(1)).grid(row=5, column=0, padx= 8, pady= 5, ipadx=5, ipady=5)
+btn1=ttk.Button(cal, text="0", command=lambda:btnClick(0)).grid(row=6, column=0, padx= 8, pady= 5, ipadx=4, ipady=5)
 
 #Second Column
-btnM=ttk.Button(cal, text=".", command=lambda:btnClick('.')).grid(row=1, column=1, padx= 8, pady= 8, ipadx=7, ipady=1)
-btn8=ttk.Button(cal, text="8", command=lambda:btnClick(8)).grid(row=2, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
-btn5=ttk.Button(cal, text="5", command=lambda:btnClick(5)).grid(row=3, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
-btn2=ttk.Button(cal, text="2", command=lambda:btnClick(2)).grid(row=4, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
-btnClear=ttk.Button(cal, text="C", style="Accent.TButton", command=btnClearDisplay).grid(row=5, column=1, padx= 10, pady= 0, ipadx=3, ipady=5)
+btnM=ttk.Button(cal, text=".", command=lambda:btnClick('.')).grid(row=2, column=1, padx= 8, pady= 8, ipadx=7, ipady=1)
+btn8=ttk.Button(cal, text="8", command=lambda:btnClick(8)).grid(row=3, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
+btn5=ttk.Button(cal, text="5", command=lambda:btnClick(5)).grid(row=4, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
+btn2=ttk.Button(cal, text="2", command=lambda:btnClick(2)).grid(row=5, column=1, padx= 0, pady= 0, ipadx=4, ipady=5)
+btnClear=ttk.Button(cal, text="C", style="Accent.TButton", command=btnClearDisplay).grid(row=6, column=1, padx= 10, pady= 0, ipadx=3, ipady=5)
 
 #Thỉrd Column
-btnM=ttk.Button(cal, text="(", command=lambda:btnClick('(')).grid(row=1, column=2, padx= 8, pady= 8, ipadx=7, ipady=1)
-btn9=ttk.Button(cal, text="9", command=lambda:btnClick(9)).grid(row=2, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
-btn6=ttk.Button(cal, text="6", command=lambda:btnClick(6)).grid(row=3, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
-btn3=ttk.Button(cal, text="3", command=lambda:btnClick(3)).grid(row=4, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
-equal=ttk.Button(cal, text="=", style="Accent.TButton", command=btnEqualsInput).grid(row=5, column=2, padx= 10, pady= 0, ipadx=2, ipady=5)
+btnM=ttk.Button(cal, text="(", command=lambda:btnClick('(')).grid(row=2, column=2, padx= 8, pady= 8, ipadx=7, ipady=1)
+btn9=ttk.Button(cal, text="9", command=lambda:btnClick(9)).grid(row=3, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
+btn6=ttk.Button(cal, text="6", command=lambda:btnClick(6)).grid(row=4, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
+btn3=ttk.Button(cal, text="3", command=lambda:btnClick(3)).grid(row=5, column=2, padx= 0, pady= 0, ipadx=4, ipady=5)
+equal=ttk.Button(cal, text="=", style="Accent.TButton", command=btnEqualsInput).grid(row=6, column=2, padx= 10, pady= 0, ipadx=2, ipady=5)
 
 #Fourth Column
-btnM=ttk.Button(cal, text=")", command=lambda:btnClick(')')).grid(row=1, column=3, padx= 8, pady= 8, ipadx=7, ipady=1)
-Addition=ttk.Button(cal, text="+",  command=lambda:btnClick("+")). grid(row=2, column=3, padx= 8, pady= 0, ipadx=3, ipady=5)
-Subtraction=ttk.Button(cal, text="-", command=lambda:btnClick("-")). grid(row=3, column=3, padx= 8, pady= 0, ipadx=4, ipady=5)
-Multiple=ttk.Button(cal, text="x", command=lambda:btnClick("*")). grid(row=4, column=3, padx= 8, pady= 0, ipadx=4, ipady=5)
-Divsion=ttk.Button(cal, text="÷", command=lambda:btnClick("/")). grid(row=5, column=3, padx= 8, pady= 0, ipadx=3, ipady=5)
+btnM=ttk.Button(cal, text=")", command=lambda:btnClick(')')).grid(row=2, column=3, padx= 8, pady= 8, ipadx=7, ipady=1)
+Addition=ttk.Button(cal, text="+",  command=lambda:btnClick("+")). grid(row=3, column=3, padx= 8, pady= 0, ipadx=3, ipady=5)
+Subtraction=ttk.Button(cal, text="-", command=lambda:btnClick("-")). grid(row=4, column=3, padx= 8, pady= 0, ipadx=4, ipady=5)
+Multiple=ttk.Button(cal, text="x", command=lambda:btnClick("*")). grid(row=5, column=3, padx= 8, pady= 0, ipadx=4, ipady=5)
+Divsion=ttk.Button(cal, text="÷", command=lambda:btnClick("/")). grid(row=6, column=3, padx= 8, pady= 0, ipadx=3, ipady=5)
 
 sv_ttk.set_theme("light")
 #Min width for the calculator
