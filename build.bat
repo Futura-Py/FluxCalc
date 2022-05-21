70ca8d99a5ea07e962502298ec5431c78b6122af
HuyHung1408 <86362423+HuyHung1408@users.noreply.github.com>
2022-05-21 21:00:13 +0700

    Prefer --onedir, faster launch time

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 cd src
 rmdir /s /q dist
-pyinstaller main.py --onefile --windowed --collect-data sv_ttk --icon "icon.ico"
+pyinstaller main.py --onedir --windowed --collect-data sv_ttk --icon "icon.ico"
 del main.spec
 xcopy icon.ico dist
 cd dist
