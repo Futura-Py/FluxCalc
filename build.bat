50baf4e90a8cb0941267c78c54a4ed78180f28ab
HuyHung1408 <86362423+HuyHung1408@users.noreply.github.com>
2023-03-26 21:17:07 +0700

    Update build.bat
    Added config.json file to build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 cd src
 rmdir /s /q dist
-pyinstaller main.py --onedir --windowed --collect-data sv_ttk --icon "icon.ico"
+pyinstaller main.py --onedir --windowed --collect-data sv_ttk --icon "icon.ico" --add-data "config.json;."
 del main.spec
 xcopy icon.ico dist
 cd dist
